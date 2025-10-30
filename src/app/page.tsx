7269529e4e9a4c3d91c08e37645a0668ca35e5a0
iLyhaha <gollfpd@gmail.com>
2025-10-30 14:16:50 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-100 dark:bg-black font-sans">
-      <main className="flex flex-col items-center justify-center text-center px-8 py-16">
+      {/* <main className="flex flex-col items-center justify-center text-center px-8 py-16">
         <h1 className="text-4xl font-bold text-black dark:text-white mb-4">
           🛠️ Ведуться технічні роботи
         </h1>
@@ -9,7 +9,7 @@ export default function Home() {
           Ми оновлюємо сайт, щоб зробити його ще кращим. Будь ласка, поверніться
           трохи пізніше — сайт скоро запрацює!
         </p>
-      </main>
+      </main> */}
     </div>
   );
 }
