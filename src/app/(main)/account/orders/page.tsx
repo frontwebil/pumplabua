ff448760b11d10287d6989b87e95cb19337e1c66
iLyhaha <gollfpd@gmail.com>
2025-11-27 15:04:14 +0200

    Production pay

--- src/app/(main)/account/orders/page.tsx
+++ src/app/(main)/account/orders/page.tsx
@@ -1,8 +1,19 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { AccauntOrdersContent } from "@/components/AccountComponents/AccauntOrdersContent";
 import { Breadcrumbs } from "@/components/Breadcrumbs/Breadcrumbs";
 import { HeroTemplateSmall } from "@/components/HeroTemplate/HeroTemplateSmall";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(authOptions);
+
+  const orders = await prisma.order.findMany({
+    where: {
+      email: session?.user.email,
+    },
+  });
+
   return (
     <>
       <HeroTemplateSmall
