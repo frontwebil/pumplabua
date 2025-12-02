e88d951ef38945cc77e129a4650d12211b2ec64c
iLyhaha <gollfpd@gmail.com>
2025-12-02 22:25:58 +0200

    update Breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -20,39 +20,44 @@ export function Breadcrumbs({ links }: Props) {
   const router = useRouter();
   const dispatch = useDispatch();
 
-  const hanleRouteReplace = (category: string, href: string) => {
-    if (category) {
-      dispatch(setFiltersFromLink([category]));
+  const hanleRouteReplace = (item: BreadcrumbItem) => {
+    // ✅ якщо є category — застосовуємо фільтр
+    if (item.category) {
+      dispatch(setFiltersFromLink([item.category]));
     }
 
-    router.replace(href);
+    // ✅ якщо є href — навігація
+    if (item.href) {
+      router.replace(item.href);
+    }
   };
 
   return (
     <div className="breadcrumbs">
       <div className="container">
         <nav className="breadcrumbs-row">
-          {links.map((item, index) =>
-            index < links.length - 1 ? (
-              <button
-                onClick={() => hanleRouteReplace(item.category, item.href)}
-                key={index}
-                className="breadcrumb-item cursor-pointer"
-              >
-                {item.title}
-                {index < links.length - 1 && (
-                  <MdKeyboardArrowRight className="breadcrumb-separator" />
-                )}
-              </button>
-            ) : (
+          {links.map((item, index) => {
+            const isLast = index === links.length - 1;
+            const isClickable = !!item.href || !!item.category;
+
+            return isLast ? (
               <div key={index} className="breadcrumb-item active">
                 {item.title}
-                {index < links.length - 1 && (
-                  <MdKeyboardArrowRight className="breadcrumb-separator" />
-                )}
               </div>
-            )
-          )}
+            ) : (
+              <button
+                key={index}
+                onClick={() => hanleRouteReplace(item)}
+                className={`breadcrumb-item ${
+                  isClickable ? "cursor-pointer" : ""
+                }`}
+                disabled={!isClickable}
+              >
+                {item.title}
+                <MdKeyboardArrowRight className="breadcrumb-separator" />
+              </button>
+            );
+          })}
         </nav>
       </div>
     </div>
